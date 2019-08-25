1a19b64625b88addb44b8e1291fdded0587fa9fb
erik <erik10cavazos@gmail.com>
2019-08-25 13:33:35 -0500

    Agregado: pagina para modificar datos de cuenta

---
+++ Pages/admin/ModificarInfoPerfil.cshtml.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using erik_tech.Models;
+
+namespace erik_tech.Pages
+{
+    public class ModificarInfoPerfil : PageModel
+    {
+        private readonly DbContextApp contexto;
+
+        public ModificarInfoPerfil(DbContextApp contexto)
+        {
+            this.contexto = contexto;
+        }
+        public void OnGet()
+        {
+            
+        }
+        public void OnPost(string id, string nuevoNombre, string nuevoCorreo, string pass)
+        {
+            Cuenta usuario = contexto.cuenta.Single(c => c.Id.Equals(int.Parse(id)));
+            usuario.username = nuevoNombre;
+            usuario.email = nuevoCorreo;
+            contexto.cuenta.Update(usuario);
+            contexto.SaveChanges();
+        }
+    }
+}
