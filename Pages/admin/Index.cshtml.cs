3dd6c13a95f3484b7874a8c97d7f9be4ae94659e
erik <erik10cavazos@gmail.com>
2019-08-25 13:24:54 -0500

    Modificacion: se abre pagina home, no se renderiza contenido en pagina admin con document.write().

--- Pages/admin/Index.cshtml.cs
+++ Pages/admin/Index.cshtml.cs
@@ -4,8 +4,24 @@ namespace erik_tech.Pages
 {
     public class AdminIndexModel : PageModel
     {
-        public void OnGet(){
+        public bool usuarioNoExiste = false;
+        public bool contraIncorrecta = false;
+        public string textoUsuario = "";
+        public void OnGet(string err="", string usuario = "")
+        {
+            switch (err)
+            {
+                case "contra-mala": contraIncorrecta = true;
+                    break;
+                case "nousuario": usuarioNoExiste = true;
+                    break;
+                default: usuarioNoExiste = false;
+                    contraIncorrecta = false;
+                    break;
+            }
 
+            if (!string.IsNullOrEmpty(usuario))
+                textoUsuario = usuario;
         }
         
 
--- Pages/admin/home.cshtml.cs
+++ Pages/admin/home.cshtml.cs
@@ -1,8 +1,11 @@
+using System;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc;
 using erik_tech.Models;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 
 namespace erik_tech.Pages
 {
@@ -19,19 +22,37 @@ namespace erik_tech.Pages
         }
         public IActionResult OnPost(string username,string password)
         {
-            RecuperarDatos(username,password);
+            try
+            {
+                Cuenta usuario = contexto.cuenta.Single(u => u.username.Equals(username));
+                if (usuario.password == password)
+                {
+                    RecuperarDatos(username,password);
+                    return Page();
+                }
+                else
+                {
+                    return RedirectToPage("/admin/Index", new{err = "contra-mala", usuario = username});
+                }
+            }
+            catch (Exception e)
+            {
+                return RedirectToPage("/admin/Index",new {err = "nousuario"});
+            }
+            
+            
             
 
-            return Page();
+            
         }
         //recuperar todos los datos del usuario ademas del nombre y la contraseña
         public void RecuperarDatos(string name, string password)
         {
             //Definitivamente el usuario existe y la contraseña es correcta.
             //Es la segunda validación, la primera fue hecha directamente por el cliente
-            if(contexto.cuenta.Where(u => u.username.Equals(name) && u.password.Equals(password)).Any())
+            if(contexto.cuenta.Any(u => u.username.Equals(name) && u.password.Equals(password)))
             {
-                var usuario = contexto.cuenta.Where(u => u.username.Equals(name) && u.password.Equals(password)).Single();
+                var usuario = contexto.cuenta.Single(u => u.username.Equals(name) && u.password.Equals(password));
                 
                 ViewData["nombre_de_usuario"] = name;
                 ViewData["password_usuario"] = password;
